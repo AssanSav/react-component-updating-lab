b039f2dd3b8fe1f5fb5c0cc5cc375498f448cd7c
AssanSav <assane.savadogo81@gmail.com>
2020-04-15 19:09:42 -0400

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -23,7 +23,24 @@ class Timer extends Component {
     clearInterval(this.interval);
   }
 
+  componentDidUpdate() {
+    this.handleBeforUpdate()
+  }
+
+  handleBeforUpdate = () => {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time !== nextState.time) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   render() {
+    // console.log("reft",this.timer.current)
     const { time, color, logText } = this.state;
     return (
       <section className="Timer" style={{ background: color }} ref={this.timer}>
